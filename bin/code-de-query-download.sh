d83750049d25a03780bfe0d91dbeb6c35f096a11
Nico Mandery <nico.mandery@dlr.de>
2017-03-14 07:16:23 +0100

    change the opensearch hostname to catalog.code-de.org. Fixes #1

--- bin/code-de-query-download.sh
+++ bin/code-de-query-download.sh
@@ -9,10 +9,10 @@
 #
 function usage {
   echo "USAGE:"
-  echo "$0 -c|--condition=... [-b|--baseurl=https://code-de.org] [-l|--limit=50] [-p|--parallel=1]"
+  echo "$0 -c|--condition=... [-b|--baseurl=https://catalog.code-de.org] [-l|--limit=50] [-p|--parallel=1]"
   echo "  --condition is the full OpenSearch query, for example:"
   echo "    -c 'parentIdentifier=EOP:CODE-DE:S2_MSI_L1C&startDate=2017-01-04T00:00:00.000Z&endDate=2017-01-04T23:59:59.999&bbox=5.9,47.2,15.2,55'"
-  echo "  --baseurl of the CODE-DE services (default is https://code-de.org)"
+  echo "  --baseurl of the CODE-DE services (default is https://catalog.code-de.org)"
   echo "  --limit the amount of products to be retrieved (default=50, max=500)"
   echo "  --parallel count of retrievals, WARNING: do not overload your system and network (the server might limit you to 2 or 4 parallel downloads)"
   echo ""
@@ -22,7 +22,7 @@ function usage {
 }
 
 #defaults
-baseUrl=https://code-de.org
+baseUrl=https://catalog.code-de.org
 batchSize=50
 parallel=1
 
