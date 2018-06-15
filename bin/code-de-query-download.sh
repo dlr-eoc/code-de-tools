c9422cf4002e590ef315db46d7d837ec83f69a1f
reck <Christoph.Reck@dlr.de>
2018-06-15 18:42:47 +0200

    Enhanced code-de-query-doenload.sh with options --user, --curlOptions and --noTransfer and fixed batchSize limit

--- bin/code-de-query-download.sh
+++ bin/code-de-query-download.sh
@@ -11,12 +11,15 @@
 #
 function usage {
   echo "USAGE:"
-  echo "$0 -c|--condition=... [-b|--baseurl=https://catalog.code-de.org] [-l|--limit=50] [-p|--parallel=1]"
+  echo "$0 -c|--condition=... [-b|--baseurl=https://catalog.code-de.org] [-o|--curlOpts=curl-options] [-l|--limit=50] [-p|--parallel=1] [-n|--noTransfer]"
   echo "  --condition is the full OpenSearch query, for example:"
-  echo "    -c 'parentIdentifier=EOP:CODE-DE:S2_MSI_L1C&startDate=2017-01-04T00:00:00.000Z&endDate=2017-01-04T23:59:59.999&bbox=5.9,47.2,15.2,55'"
+  echo "    -c 'parentIdentifier=EOP:CODE-DE:S2_MSI_L1C&startDate=2018-06-04T00:00:00.000Z&endDate=2018-06-04T23:59:59.999&bbox=5.9,47.2,15.2,55'"
+  echo "  --user in the form username:password (alternatively use --curlOpts='--netrc-file...myNetRc...file'"
   echo "  --baseurl of the CODE-DE services (default is https://catalog.code-de.org)"
+  echo "  --curlOpts allos specifying special curl options like -o='--progress-bar --netrc-file=...myNetRc...file'"  
   echo "  --limit the amount of products to be retrieved (default=50, max=500)"
   echo "  --parallel count of retrievals, WARNING: do not overload your system and network (the server might limit you to 2 or 4 parallel downloads)"
+  echo "  --noTransfer to test the query"
   echo ""
   echo "Output products are placed in the current directory."
   echo ""
@@ -24,23 +27,31 @@ function usage {
 }
 
 #defaults
+user=''
 baseUrl=https://catalog.code-de.org
+curlOpts=''
 batchSize=50
 parallel=1
+noExec=''
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
     -c|--condition) condition="$2"; shift 2;;
+    -u|--user)      user="--cookie-jar /tmp/$(basename $0)_$$ --location-trusted --user $2"; shift 2;;
     -b|--baseurl)   baseUrl="$2"; shift 2;;
-    -l|--limit)     limit="$2"; shift 2;;
+    -o|--curlOpts)  curlOpts="$2"; shift 2;;
+    -l|--limit)     batchSize="$2"; shift 2;;
     -p|--parallel)  parallel="$2"; shift 2;;
+    -n|--noTransfer) noExec="echo"; shift 1;;
 
     -c=*|--condition=*) condition="${1#*=}"; shift 1;;
+    -u=*|--user=*)      user="--cookie-jar /tmp/$(basename $0)_$$ --location-trusted --user ${1#*=}"; shift 1;;
     -b=*|--baseurl=*)   baseUrl="${1#*=}"; shift 1;;
-    -l=*|--limit=*)     limit="${1#*=}"; shift 1;;
+    -o=*|--curlOpts=*)  curlOpts="${1#*=}"; shift 1;;
+    -l=*|--limit=*)     batchSize="${1#*=}"; shift 1;;
     -p=*|--parallel=*)  parallel="${1#*=}"; shift 1;;
 
-    *) echo "Unknown option '$1'"; usage; exit;;
+    *) echo "ERROR: unknown option '$1'"; usage; exit;;
   esac
 done
  
@@ -56,7 +67,7 @@ echo "Running query with $condition"
 searchUrl=$baseUrl/opensearch/request/?httpAccept=application/atom%2Bxml
 
 # execute query and extract the dwnload URL list
-urls=$(curl -s "${searchUrl}&${condition}&maximumRecords=${batchSize}" | xmllint --xpath '//*[local-name()="link" and @title="Download"]/@href' - |sed -e 's/ *href="//g' | tr '"' '\n' )
+urls=$(curl -s $curlOpts "${searchUrl}&${condition}&maximumRecords=${batchSize}" | xmllint --xpath '//*[local-name()="link" and @title="Download"]/@href' - |sed -e 's/ *href="//g' | tr '"' '\n' )
 
 count=$(echo $urls | wc -w | tr -d ' ')
 if [ $count = 0 ]; then
@@ -67,5 +78,4 @@ else
 fi
 
 # download them all to the local directory
-echo $urls | xargs -n1 -P${parallel} curl -O
-
+echo $urls | xargs -n1 -P${parallel} $noExec curl $user $curlOpts -O
